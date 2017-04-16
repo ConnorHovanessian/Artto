980382071b6e65bdeb6849c6bc32885af06484db
Dima <dima.goncharov@hotmail.com>
2017-04-16 09:00:20 +0000

    added art submission

--- public/js/drawingApp.js
+++ public/js/drawingApp.js
@@ -1,4 +1,55 @@
-LC.init(
+var lc = LC.init(
     document.getElementsByClassName('my-drawing')[0],
     {imageURLPrefix: '/literallycanvas/img'}
 );
+
+window.onload = function () {
+    var oneMinute = 60,
+    display = document.querySelector('#time');
+    display.textContent = oneMinute; 
+    startTimer(oneMinute, display);
+    setInterval(submitArt, 5000);
+};
+
+
+function submitArt()
+{
+    
+    var form = document.querySelector('#imageForm');
+    var imgText = document.querySelector('#img');
+    console.log(lc.getImage().toDataURL());
+    imgText.value = lc.getImage().toDataURL();
+    form.submit();
+}
+
+function startTimer(duration, display) {
+    var start = Date.now(),
+        diff,
+        seconds;
+    function timer() {
+        // get the number of seconds that have elapsed since 
+        // startTimer() was called
+        diff = duration - (((Date.now() - start) / 1000) | 0);
+
+        // does the same job as parseInt truncates the float
+        seconds = (diff % 60) | 0;
+
+        seconds = seconds < 10 ? "6" + seconds : seconds;
+        
+        if(seconds === 1)
+        {
+             submitArt();
+        }
+        
+        display.textContent = seconds; 
+
+        if (diff <= 0) {
+            // add one second so that the count down starts at the full duration
+            // example 05:00 not 04:59
+            start = Date.now() + 1000;
+        }
+    };
+    // we don't want to wait a full second before the timer starts
+    timer();
+    setInterval(timer, 1000);
+}
--- routes/art.js
+++ routes/art.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var User = require("../models/user");
 var passport = require("passport");
 var middleware = require("../middleware");
+var fs = require('fs');
 
 
 
@@ -13,8 +14,23 @@ router.get("/art", middleware.isLoggedIn, function(req, res){
 
 router.post("/art/:userID", middleware.isLoggedIn, function(req, res){
     
-    res.send(":)");
-
+    var img = req.body.img;
+    var data = img.replace(/^data:image\/\w+;base64,/, "");
+    var buf = new Buffer(data, 'base64');
+    fs.writeFile(__dirname + '/../public/submissions/image.png', buf, 'base64', function(err){
+        if(err)
+        {
+            req.flash("error", "Error with submission, please try again!" + err);
+            res.redirect("/home");
+        }
+        else
+        {
+            
+            req.flash("success", "Art submission successfull!");
+            res.redirect("/home");
+        }
+    });
+    
 });
 
 module.exports = router;
