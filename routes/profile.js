7b62744c7c9f7be369b57550ccb1ef41d15bc8d2
Connor Hovanessian <chovanes@sfu.ca>
2017-05-24 08:23:23 +0000

    Differentiated forgot password pw reset from signed-in pw reset, created corresponding routes for profile reset

--- routes/profile.js
+++ routes/profile.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var User = require("../models/user");
 var middleware = require("../middleware");
 var constants = require("../util/constants");
+var passport = require("passport");
 
 
 //Route to render user profile information
@@ -18,8 +19,7 @@ router.get("/changePassword", middleware.isLoggedIn, function(req, res){
     res.render("profile/changePassword");
     
 });
-
-//Route to handle changing of passwords
+//Route to handle changing of passwords from "forgot password"
 router.post("/changePassword", middleware.isLoggedIn, function(req, res){
     
     //Check to make sure user data is valid
@@ -39,6 +39,49 @@ router.post("/changePassword", middleware.isLoggedIn, function(req, res){
         else
         {
             user.setPassword(req.body.password, function(){
+                user.save();
+                req.flash("success", "Your password was successfully changed!");
+                return res.redirect("/profile");
+            });
+        }
+    
+    });
+    
+});
+
+
+//Route to render change password form
+router.get("/changePasswordProfile", middleware.isLoggedIn, function(req, res){
+    
+    res.render("profile/changePasswordProfile");
+    
+});
+
+//Route to handle changing of passwords from profile
+router.post("/changePasswordProfile", middleware.isLoggedIn, passport.authenticate("local", {
+    failureRedirect: "/changePasswordProfile",
+    failureFlash: true
+    }),
+    
+    function(req, res){
+    
+    //Check to make sure user data is valid
+    if(!constants.passwordRegex.test(req.body.newpassword)
+        || (req.body.newpassword !== req.body.newpassword_confirmation))
+    {
+        req.flash("error", "Invalid passwords detected!");
+        return res.redirect("/changePasswordProfile");
+    }
+    
+    User.findById(req.user._id, function(err, user){
+
+        if(err)
+        {
+            return res.redirect("/");
+        }
+        else
+        {
+            user.setPassword(req.body.newpassword, function(){
                 user.save();
                 req.flash("success", "Your password was successfully changed!");
                 return res.redirect("/profile");
